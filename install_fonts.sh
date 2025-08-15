5127bab460b719f9837135f3b3e856698266e52f
D A R K <abdu.ewian@gmail.com>
2025-08-15 19:28:43 +0000

    add

--- install_fonts.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Installing Arabic fonts for Mobile Shop Management System..."
-echo "تثبيت الخطوط العربية لنظام إدارة محل الموبايلات..."
-
-python3 setup_fonts.py
-
-echo "Done!"
-echo "تم الانتهاء!"
